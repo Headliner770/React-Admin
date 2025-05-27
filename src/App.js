d0f8f5338a8d2fa2be9eabb7109c9b82404ef524
Headliner770 <paul248022@gmail.com>
2025-05-27 10:23:31 +0300

    theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
-const App = () => {
+function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div>App</div>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
